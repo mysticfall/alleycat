28d84195db495b16fc41c1d005531cd4843c33e8
Xavier Cho <mysticfallband@gmail.com>
2026-02-14 18:49:07 +0900

    Extract `RigFactory<TBone>` base class from `HumanRigFactory`.

---
+++ src/Rig/RigFactory.cs
@@ -0,0 +1,31 @@
+using AlleyCat.Common;
+using AlleyCat.Env;
+using AlleyCat.Service;
+using AlleyCat.Service.Typed;
+using Godot;
+using LanguageExt;
+using Microsoft.Extensions.Logging;
+
+namespace AlleyCat.Rig;
+
+public abstract partial class RigFactory<TBone> : NodeFactory<IRig<TBone>>, IServiceFactory
+    where TBone : struct, Enum
+{
+    [Export] public Skeleton3D? Skeleton { get; set; }
+
+    //Need eager initialisation to trigger idle animations (i.e. ShowRestOnly = false).
+    InstantiationOption IServiceFactory.Instantiation => InstantiationOption.Singleton;
+
+    protected override Eff<IEnv, IRig<TBone>> CreateService(
+        ILoggerFactory loggerFactory
+    ) =>
+        from skeleton in Skeleton.Require("Skeleton is not set.")
+        from _ in IO.lift(() => skeleton.ShowRestOnly = false)
+        from rig in CreateService(skeleton, loggerFactory)
+        select rig;
+
+    protected abstract Eff<IEnv, IRig<TBone>> CreateService(
+        Skeleton3D skeleton,
+        ILoggerFactory loggerFactory
+    );
+}
