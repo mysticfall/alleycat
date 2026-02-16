ab37ca14501c2eacf471317e410d87f2d8174ee2
Xavier Cho <mysticfallband@gmail.com>
2026-02-16 15:55:20 +0900

    Add IK pole targets and knee pole calculation to `AdjustLegsIk`.

--- src/Rig/Human/Modifier/AdjustLegsIk.cs
+++ src/Rig/Human/Modifier/AdjustLegsIk.cs
@@ -2,30 +2,83 @@ using AlleyCat.Env;
 using AlleyCat.Logging;
 using AlleyCat.Rig.Ik;
 using AlleyCat.Transform;
+using Godot;
 using LanguageExt;
 using Microsoft.Extensions.Logging;
 using static LanguageExt.Prelude;
 
 namespace AlleyCat.Rig.Human.Modifier;
 
-public class AdjustLegsIk(
-    IMovable3d rightFootTarget,
-    IMovable3d leftFootTarget,
-    IRig<HumanBone> rig,
-    IObservable<Duration> onIkProcess,
-    ILoggerFactory? loggerFactory = null
-) : IIkModifier
+public class AdjustLegsIk : IIkModifier
 {
-    public IObservable<Duration> OnIkProcess => onIkProcess;
+    public IObservable<Duration> OnIkProcess { get; }
 
-    public ILogger Logger { get; } = loggerFactory.GetLogger<AdjustLegsIk>();
+    public ILogger Logger { get; }
 
-    public ILoggerFactory? LoggerFactory => loggerFactory;
+    public ILoggerFactory? LoggerFactory { get; }
 
-    public Eff<IEnv, Unit> Process(Duration delta) =>
-        from animRightFoot in rig.GetGlobalPose(HumanBone.RightFoot)
-        from animLeftFoot in rig.GetGlobalPose(HumanBone.LeftFoot)
-        from _1 in rightFootTarget.SetGlobalTransform(animRightFoot)
-        from _2 in leftFootTarget.SetGlobalTransform(animLeftFoot)
-        select unit;
+    private readonly Eff<Unit> _process;
+
+    private readonly Length _poleLength;
+
+    public AdjustLegsIk(
+        IMovable3d rightFootTarget,
+        IMovable3d leftFootTarget,
+        IMovable3d rightKneePole,
+        IMovable3d leftKneePole,
+        IRig<HumanBone> rig,
+        IObservable<Duration> onIkProcess,
+        Length? poleLength = null,
+        ILoggerFactory? loggerFactory = null
+    )
+    {
+        OnIkProcess = onIkProcess;
+
+        Logger = loggerFactory.GetLogger<AdjustLegsIk>();
+        LoggerFactory = loggerFactory;
+
+        _poleLength = poleLength ?? 1.Metres();
+
+        _process =
+            from toSkeleton in rig.GlobalTransform
+            from _1 in SyncLeg(
+                toSkeleton,
+                HumanBone.RightUpperLeg,
+                HumanBone.RightLowerLeg,
+                HumanBone.RightFoot,
+                rightFootTarget,
+                rightKneePole
+            )
+            from _2 in SyncLeg(
+                toSkeleton,
+                HumanBone.LeftUpperLeg,
+                HumanBone.LeftLowerLeg,
+                HumanBone.LeftFoot,
+                leftFootTarget,
+                leftKneePole
+            )
+            select unit;
+
+        return;
+
+        Eff<Unit> SyncLeg(
+            Transform3D toSkeleton,
+            HumanBone upperLegBone,
+            HumanBone lowerLegBone,
+            HumanBone footBone,
+            IMovable3d footTarget,
+            IMovable3d poleTarget
+        ) =>
+            from upperLeg in rig.GetPose(upperLegBone)
+            from lowerLeg in rig.GetPose(lowerLegBone)
+            from foot in rig.GetPose(footBone)
+            let poleDir = lowerLeg.Basis * Vector3.Forward
+            let poleOrigin = lowerLeg.Origin + poleDir * (float)_poleLength.Metres
+            let pole = new Transform3D(Basis.Identity, poleOrigin)
+            from _1 in footTarget.SetGlobalTransform(toSkeleton * foot)
+            from _2 in poleTarget.SetGlobalTransform(toSkeleton * pole)
+            select unit;
+    }
+
+    public Eff<IEnv, Unit> Process(Duration delta) => _process;
 }
--- src/Rig/Human/Modifier/AdjustLegsIkFactory.cs
+++ src/Rig/Human/Modifier/AdjustLegsIkFactory.cs
@@ -11,9 +11,15 @@ namespace AlleyCat.Rig.Human.Modifier;
 [GlobalClass]
 public partial class AdjustLegsIkFactory : HumanIkModifierFactory
 {
-    [Export] public Node3D? RightFootTarget { get; set; }
+    [ExportGroup("IK Targets")] [Export] public Node3D? RightFootTarget { get; set; }
 
-    [Export] public Node3D? LeftFootTarget { get; set; }
+    [Export] public Marker3D? LeftFootTarget { get; set; }
+
+    [ExportGroup("IK Poles")] [Export] public Node3D? RightKneePole { get; set; }
+
+    [Export] public Marker3D? LeftKneePole { get; set; }
+
+    [Export(PropertyHint.Range, "0.1,3")] public float PoleLength { get; set; } = 1f;
 
     protected override Eff<IEnv, IIkModifier> CreateService(
         IRig<HumanBone> rig,
@@ -22,11 +28,16 @@ public partial class AdjustLegsIkFactory : HumanIkModifierFactory
     ) =>
         from rightFoot in RightFootTarget.Require("Right foot target is not set.")
         from leftFoot in LeftFootTarget.Require("Left foot target is not set.")
+        from rightKnee in RightKneePole.Require("Right knee pole is not set.")
+        from leftKnee in LeftKneePole.Require("Left knee pole is not set.")
         select (IIkModifier)new AdjustLegsIk(
             rightFoot.AsMovable(),
             leftFoot.AsMovable(),
+            rightKnee.AsMovable(),
+            leftKnee.AsMovable(),
             rig,
             onIkProcess,
+            PoleLength.Metres(),
             loggerFactory
         );
 }
