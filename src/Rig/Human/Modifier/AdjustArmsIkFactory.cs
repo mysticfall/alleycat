ed58152571eed0454e469dc7f5a3700fa1f114a6
Xavier Cho <mysticfallband@gmail.com>
2026-02-18 19:59:24 +0900

    Add shoulder IK targets and improve arm IK pole calculation.

--- src/Rig/Human/Modifier/AdjustArmsIkFactory.cs
+++ src/Rig/Human/Modifier/AdjustArmsIkFactory.cs
@@ -15,6 +15,10 @@ public partial class AdjustArmsIkFactory : HumanIkModifierFactory
 
     [Export] public Node3D? LeftHand { get; set; }
 
+    [ExportGroup("IK Targets")] [Export] public Node3D? RightShoulder { get; set; }
+
+    [Export] public Node3D? LeftShoulder { get; set; }
+
     [ExportGroup("IK Poles")] [Export] public Node3D? RightElbowPole { get; set; }
 
     [Export] public Marker3D? LeftElbowPole { get; set; }
@@ -28,11 +32,15 @@ public partial class AdjustArmsIkFactory : HumanIkModifierFactory
     ) =>
         from rightHand in RightHand.Require("Right hand is not set.")
         from leftHand in LeftHand.Require("Left hand is not set.")
+        from rightShoulder in RightShoulder.Require("Right shoulder target is not set.")
+        from leftShoulder in LeftShoulder.Require("Left shoulder target is not set.")
         from rightElbow in RightElbowPole.Require("Right elbow pole is not set.")
         from leftElbow in LeftElbowPole.Require("Left elbow pole is not set.")
         select (IIkModifier)new AdjustArmsIk(
-            rightHand.AsMovable(),
-            leftHand.AsMovable(),
+            rightHand.AsLocatable(),
+            leftHand.AsLocatable(),
+            rightShoulder.AsMovable(),
+            leftShoulder.AsMovable(),
             rightElbow.AsMovable(),
             leftElbow.AsMovable(),
             rig,
