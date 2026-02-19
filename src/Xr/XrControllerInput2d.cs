00f7a24779a6fef08e092d3408b62a46f1901520
Xavier Cho <mysticfallband@gmail.com>
2026-02-19 23:51:08 +0900

    Add XR controller 2D input support.

---
+++ src/Xr/XrControllerInput2d.cs
@@ -0,0 +1,14 @@
+using AlleyCat.Control;
+using Godot;
+using LanguageExt;
+
+namespace AlleyCat.Xr;
+
+public class XrControllerInput2d(
+    InputEventName eventName,
+    XRController3D controller,
+    IObservable<Duration> onProcess
+) : Input2d(onProcess)
+{
+    protected override Vector2 Process(Duration duration) => controller.GetVector2(eventName);
+}
