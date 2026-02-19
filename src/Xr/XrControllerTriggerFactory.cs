cc3078fc64b4584b30663374761b910c78455b46
Xavier Cho <mysticfallband@gmail.com>
2026-02-19 23:46:13 +0900

    Replace `XrControllerSide` enum with shared `Side` enum.

--- src/Xr/XrControllerTriggerFactory.cs
+++ src/Xr/XrControllerTriggerFactory.cs
@@ -11,14 +11,14 @@ namespace AlleyCat.Xr;
 [GlobalClass]
 public partial class XrControllerTriggerFactory : TriggerFactory
 {
-    [Export] public XrControllerSide Side { get; set; } = XrControllerSide.Right;
+    [Export] public Side Side { get; set; } = Side.Right;
 
     [Export] public string? EventName { get; set; } = "trigger_click";
 
     protected override Eff<IEnv, ITrigger> CreateService(ILoggerFactory loggerFactory) =>
         from xr in service<XrDevices>()
         let trackers = xr.Trackers
-        let tracker = Side == XrControllerSide.Right ? trackers.RightHand : trackers.LeftHand
+        let tracker = Side == Side.Right ? trackers.RightHand : trackers.LeftHand
         from action in InputEventName.Create(EventName).ToEff(identity)
         select (ITrigger)new XrControllerTrigger(action, tracker.Controller, loggerFactory);
 }
