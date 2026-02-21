168028bed2ce1592dba2a7ab4d30869d2529e829
Xavier Cho <mysticfallband@gmail.com>
2026-02-21 18:56:56 +0900

    Add `FindService` and `GetService` extension methods for Node.
    Added extension methods to search for services in node descendants. Refactored `RunIfNotReady` to use explicit type parameter in `Match` call.

--- src/Service/IServiceFactory.cs
+++ src/Service/IServiceFactory.cs
@@ -1,7 +1,9 @@
+using AlleyCat.Common;
 using AlleyCat.Env;
+using Godot;
 using LanguageExt;
-using LanguageExt.Common;
 using static LanguageExt.Prelude;
+using Error = LanguageExt.Common.Error;
 
 namespace AlleyCat.Service;
 
@@ -23,14 +25,34 @@ public interface IServiceFactory<TService> : IServiceFactory
 
 public static class ServiceFactoryExtensions
 {
+    extension(Node node)
+    {
+        public Eff<IEnv, Option<T>> FindService<T>() =>
+            node
+                .GetDescendants()
+                .OfType<IServiceFactory<T>>()
+                .AsIterable()
+                .Head
+                .Traverse(x => x.TypedService)
+                .As();
+
+        public Eff<IEnv, T> GetService<T>() =>
+            node
+                .FindService<T>()
+                .Bind(x => x.ToEff(
+                    Error.New(
+                        $"No service of type {typeof(T).FullName} found under node {node.GetPath()}."
+                    )
+                ));
+    }
+
     internal static Fin<T> RunIfNotReady<T>(this Fin<T> instance, Eff<IEnv, T> service) =>
         instance | @catch(e => e is NotReadyError,
-            Optional(GodotEnv.Instance)
-                .Match(
-                    env => service.Run(env),
-                    () => Error.New(
-                        $"GodotEnv.Instance is null, cannot create service {typeof(T)}."
-                    )
+            Optional(GodotEnv.Instance).Match<Fin<T>>(
+                env => service.Run(env),
+                () => Error.New(
+                    $"GodotEnv.Instance is null, cannot create service {typeof(T)}."
                 )
+            )
         );
 }
